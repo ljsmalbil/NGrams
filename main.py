f7f649c0641574b2814454f3288d93e7ce1f76d7
ljsmalbil <louksmalbil@gmail.com>
2020-11-13 16:41:24 +0100

    small update

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# https://github.com/minimaxir/textgenrnn
 from textgenrnn import textgenrnn
-
 
 textgen_2 = textgenrnn('textgenrnn_weights.hdf5')
 #textgen_2.generate(1, temperature=0.1)
 
 #textgen_2.generate(interactive=True, top_n=5)
 
-textgen_2.generate_samples()
+#textgen_2.generate_samples()
+
+#textgen_2.generate(10, temperature=0.3)
+
+textgen_2.generate(interactive=True)
 
 
 
